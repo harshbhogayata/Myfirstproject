64d97b54275b1da652b892b2ebeacce151f00f58
Harsh Bhogayata <harshmbhogayata@gmail.com>
2024-09-21 19:44:20 +0530

    Update Index.php

--- Index.php
+++ Index.php
@@ -19,3 +19,8 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     }
 }
 ?>
+<form method="POST" action="">
+    <input type="email" name="email" placeholder="Email" required>
+    <input type="password" name="password" placeholder="Password" required>
+    <button type="submit">Login</button>
+</form>
